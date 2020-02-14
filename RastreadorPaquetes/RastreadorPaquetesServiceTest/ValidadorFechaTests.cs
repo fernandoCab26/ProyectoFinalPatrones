0c73f874ac3944c2db483229895627c4358be1a9
Fernando Cab <fernando.cab@blueoceantech.com.mx>
2020-02-14 10:11:33 -0600

    subiendo ultimos cambios

---
+++ RastreadorPaquetes/RastreadorPaquetesServiceTest/ValidadorFechaTests.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RastreadorPaquetesService;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RastreadorPaquetesService.Tests
+{
+    [TestClass()]
+    public class ValidadorFechaTests
+    {
+
+        [TestMethod]
+        public void ValidarFechaActualTest_FechaMenorActual_DevuelveTrue()
+        {
+            //Arrange
+            ValidadorFecha validadorFecha = new ValidadorFecha();
+            DateTime fechamenor = DateTime.Now.AddDays(-1);
+            //Act
+           bool act =validadorFecha.ValidarFechaActual(fechamenor);
+            //Assert
+            Assert.IsTrue(act);
+        }
+
+        [TestMethod]
+        public void ValidarFechaActualTest_FechaMenorActual_DevuelveFalse()
+        {
+            //Arrange
+            ValidadorFecha validadorFecha = new ValidadorFecha();
+            DateTime fechamenor = DateTime.Now.AddDays(1);
+            //Act
+            bool act = validadorFecha.ValidarFechaActual(fechamenor);
+            //Assert
+            Assert.IsFalse(act);
+        }
+
+    }
+}
---
+++ RastreadorPaquetes/RastreadorPaquetesServiceTests/ManejadorRangoTiempoTests.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RastreadorPaquetesService;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RastreadorPaquetesService.Tests
+{
+    [TestClass()]
+    public class ManejadorRangoTiempoTests
+    {
+        [TestMethod()]
+        public void ObtenerRangoTiempoTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
